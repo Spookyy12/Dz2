f53a6ce5cdc2531d9d194e5041e3d2de258e96d7
Spookyy12 <131680995+Spookyy12@users.noreply.github.com>
2023-06-11 23:16:19 +0300

    firstCom

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15_621;
+        int bonusMile;
+        bonusMile = (ticketPrice / 20);
+        System.out.print("Ваши бонусные мили " + bonusMile);
+    }
+}
